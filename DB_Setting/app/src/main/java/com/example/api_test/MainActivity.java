4e454e7473941691e857d00c6aba6fad68859517
gunho <parkgh3737@naver.com>
2019-08-22 16:37:45 +0900

    add SQLite DB

--- DB_Setting/app/src/main/java/com/example/api_test/MainActivity.java
+++ DB_Setting/app/src/main/java/com/example/api_test/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,13 +20,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        readBarrierFreeData();
-    }
+    EditText name, loc, desig;
+    Button saveBtn;
+    Intent intent;
 
     private List<BFDataSample> bfSamples= new ArrayList<>();
     private void readBarrierFreeData() {
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 BFDataSample sample = new BFDataSample();
                 //sample.setNum(tokens[0]);
                 sample.setBusinessName(tokens[1]);
+
                 sample.setTel(tokens[2]);
                 sample.setFax(tokens[3]);
                 sample.setAddress(tokens[4]);
@@ -59,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
             //Log.wtf("MyActivity","Error Reading Data File on Line"+line,e);
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.txtName);
+        loc = (EditText)findViewById(R.id.txtLocation);
+        desig = (EditText)findViewById(R.id.txtDesignation);
+        saveBtn = (Button)findViewById(R.id.btnSave);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = name.getText().toString()+"\n";
+                String location = loc.getText().toString();
+                String designation = desig.getText().toString();
+                DBHandler dbHandler = new DBHandler(MainActivity.this);
+                dbHandler.insertUserDetails(username,location,designation);
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Details Inserted Successfully",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        readBarrierFreeData();
     }
 }
